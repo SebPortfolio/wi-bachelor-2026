f721788ba2fa4305ee3c7d4709ae372b08d8b359
Sebastian Koch <sebpk.bln@gmail.com>
2026-07-17 11:54:26 +0200

    feat(design-first): changed ArticleMapper for new MoneyWdto

--- design-first/src/main/java/de/htwberlin/wi/designfirst/article/ArticleMapper.java
+++ design-first/src/main/java/de/htwberlin/wi/designfirst/article/ArticleMapper.java
@@ -3,14 +3,18 @@ package de.htwberlin.wi.designfirst.article;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import de.htwberlin.wi.designfirst.article.articlecategory.ArticleCategoryMapper;
 import de.htwberlin.wi.designfirst.model.ArticleWdto;
 
 @Mapper(componentModel = "spring", uses = { ArticleCategoryMapper.class })
 public interface ArticleMapper {
+    @Mapping(target = "price.amount", source = "price")
+    @Mapping(target = "price.currency", constant = "EUR")
     ArticleWdto toWdto(Article article);
 
+    @Mapping(target = "price", source = "price.amount")
     Article fromWdto(ArticleWdto articleWdto);
 
     List<ArticleWdto> toWdtoList(List<Article> articles);
